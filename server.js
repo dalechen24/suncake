7af2deb446f6b9465a23be243c44b240f414c915
DaleChen <dalechen24@gmail.com>
2015-11-08 16:52:25 +0800

    added express.static to server.js you can test static resource with imgs/suncake.jpg

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+// loading modules
 var express = require('express');
 var http = require('http');
 
+// initial server
 var server = express();
 server.set('port', process.env.PORT || 3000);
+server.use(express.static('public'));
 
+// http verbs
 server.get('/', function(req, res) {
 	res.send('Hello World from GET request.');
 });
@@ -17,13 +21,12 @@ server.delete('/user', function(req, res) {
 	res.send('Got a DELETE request at /user.');
 });
 
-
+// exception handling
 server.use(function(req, res) {
 	res.type('text/html');
 	res.status(404);
 	res.send('<h1>404 - Not Found</h1>');
 });
-
 server.use(function(err, req, res, next) {
 	console.log(err.stack);
 	res.type('text/html');
@@ -31,6 +34,7 @@ server.use(function(err, req, res, next) {
 	res.send('<h1>500 - SERVER ERROR</h1>');
 });
 
+// server listen on...
 server.listen(server.get('port'), function() {
 	console.log('Server started on localhost:' + server.get('port') + '; press Ctrl-C to terminate.');
 });
